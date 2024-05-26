998d8c1170248ff5f38e6ab5b2be261fa905a706
donghwanchung <23718894+NeonAfro@users.noreply.github.com>
2024-05-26 13:34:21 -0700

    barebones recipe

---
+++ recipe.php
@@ -0,0 +1,66 @@
+<?php
+require_once('config.php');
+$conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+$sql = "SELECT RecipeID, Title, Description, Image FROM Recipes";
+$result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Recipes List</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/sketchy/bootstrap.min.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">HusciiByte</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-
+                    controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarColor02">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.php">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="recipe.php">Recipes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile.php">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contributors.php">Contributors</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <h1>Available Recipes</h1>
+            <div class="row">
+                <?php if ($result->num_rows > 0): ?>
+                <?php while ($recipe = $result->fetch_assoc()): ?>
+                <div class="col-md-4">
+                    <div class="card mb-3">
+                        <img src="<?= htmlspecialchars($recipe['Image']) ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= htmlspecialchars($recipe['Title']) ?></h5>
+                            <p class="card-text"><?= htmlspecialchars($recipe['Description']) ?></p>
+                            <a href="recipe.php?id=<?= $recipe['RecipeID'] ?>" class="btn btn-primary">View Recipe</a>
+                        </div>
+                    </div>
+                </div>
+                <?php endwhile; ?>
+                <?php else: ?>
+                <p>No recipes found.</p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </body>
+</html>
+
+
