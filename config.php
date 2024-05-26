bdfd6da4c640991f9b8bad973bf70bfd28484638
donghwanchung <23718894+NeonAfro@users.noreply.github.com>
2024-05-26 13:37:23 -0700

    +config | home

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'HusciiByte');
+define('DBUSER', 'testuser');
+define('DBPASS', 'mypassword');
+define('DBCONNSTRING','mysql:dbname=HusciiByte;charset=utf8mb4');
+?>
